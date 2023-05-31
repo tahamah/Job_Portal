76bdbb81be993059794e732fae28d93062416b61
tahamah <taha.iu.bd@gmail.com>
2023-05-31 23:54:05 +0600

    sub btn add

---
+++ src/components/common/Subscribe/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Subscribe = () => {
+  return (
+    <div className=' border-t-2'>
+      <div class='md:flex md:justify-between mx-auto w-full max-w-screen-xl items-center py-14 '>
+        <div className=' md:flex items-center gap-10 px-5'>
+          <div className='text-center'>Img</div>
+          <div className='py-4'>
+            <h3 className='text-2xl font-sana '>Subscribe to our newsletter</h3>
+            <p className='text-normal text-gray-500'>
+              We{`'`}ll keep you updated with the best new jobs.
+            </p>
+          </div>
+        </div>
+        <div>
+          <form class=' md:flex gap-4 items-center px-6 md:px-0'>
+            <label for='simple-search' class='sr-only'>
+              Search
+            </label>
+            <div class='w-full'>
+              <input
+                type='text'
+                id='simple-search'
+                class='bg-gray-50 md:w-[300px] my-5 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block  w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                placeholder='Enter your email'
+                required
+              />
+            </div>
+            <button
+              type='submit'
+              class='p-2.5 w-full   text-sm font-medium text-white bg-blue-700 rounded-full border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Subscribe
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import HeroSection from "@/components/common/HeroSection"
 
 export default function Home() {
-  return <div><HeroSection/></div>
+  return <div><HeroSection />
+
+  </div>
 }
