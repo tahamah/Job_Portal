767644f9b3f750e2d48205cafec54f9f6c82a736
tahamah <taha.iu.bd@gmail.com>
2023-06-01 22:20:24 +0600

    find job start

---
+++ src/components/common/FindJob/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const FindJob = () => {
+  return (
+    <div>
+      <h1 className=' bg-blue-100 text-3xl text-center py-10 font-medium'>
+        Find Job
+          </h1>
+          <div className='max-w-7xl mx-auto'>
+              <div >
+              cart    
+              </div>
+              
+          </div>
+    </div>
+  )
+}
+
+export default FindJob
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Articles from "@/components/common/Articles"
+import FindJob from "@/components/common/FindJob"
 import HeroSection from "@/components/common/HeroSection"
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      <Articles/>
+      <Articles />
+      <FindJob />
     </div>
   )
 }
