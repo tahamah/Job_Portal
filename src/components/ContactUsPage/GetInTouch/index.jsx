e854a0809d511fe2f488192594ac907b674a36ef
tahamah <taha.iu.bd@gmail.com>
2023-06-03 16:24:12 +0600

    file change

---
+++ src/components/ContactUsPage/GetInTouch/index.jsx
@@ -0,0 +1,66 @@
+import TouchImg from '../../../assets/Section_Img.png'
+import { HiOutlinePhoneMissedCall } from 'react-icons/hi'
+import { BiTimeFive } from 'react-icons/bi'
+import { ImLocation } from 'react-icons/im'
+import Image from 'next/image'
+import React from 'react'
+
+const GetInTouch = () => {
+  return (
+    <div className='flex justify-center items-center gap-16'>
+      <div className='w-1/2 relative'>
+        <h2 className='text-2xl font-semibold'>Get in touch</h2>
+        <div className='w-12 h-1 mt-2 ml-3 bg-black'></div>
+        <p className='my-5 leading-8'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
+          accusamus quod error corporis asperiores illo, unde est! Nam numquam
+          sed cupiditate repellat debitis eaque sit similique.
+        </p>
+        <ul className='flex flex-col gap-2.5'>
+          <li className='flex gap-2 items-center'>
+            {' '}
+            <span className='text-red-600'>
+              <ImLocation />
+            </span>{' '}
+            457 BlgBlue Street, NY 10013
+          </li>
+          <li className='flex gap-2 items-center'>
+            {' '}
+            <span className='text-red-600'>
+              <ImLocation />
+            </span>{' '}
+            998 Some Street, LA 10013
+          </li>
+          <li className='flex gap-2 items-center'>
+            {' '}
+            <span className='text-red-600'>
+              {' '}
+              <ImLocation />
+            </span>{' '}
+            457 BlgBlue Street, NY 100143
+          </li>
+          <li className='flex gap-2 items-center'>
+            {' '}
+            <span className='text-red-600'>
+              {' '}
+              <HiOutlinePhoneMissedCall />
+            </span>{' '}
+            +44 300 303 0266{' '}
+          </li>
+          <li className='flex gap-2 items-center'>
+            {' '}
+            <span className='text-red-600'>
+              <BiTimeFive />
+            </span>{' '}
+            Mon-Sat 8.00-18.00
+          </li>
+        </ul>
+      </div>
+      <div className='w-1/2'>
+        <Image src={TouchImg} alt='' />
+      </div>
+    </div>
+  )
+}
+
+export default GetInTouch
--- src/pages/contact-us/index.jsx
+++ src/pages/contact-us/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import GetInTouch from '../../components/ContactUsPage/GetInTouch'
 
 const ContactUs = () => {
   return (
-    <div>
-
-      Contact
+    <div className='max-w-7xl mx-auto'>
+      <GetInTouch />
     </div>
   )
 }
