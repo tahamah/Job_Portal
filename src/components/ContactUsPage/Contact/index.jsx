0fb9a67dfb2d6514005f82ff5f4504734ddf731b
tahamah <taha.iu.bd@gmail.com>
2023-06-04 11:48:50 +0600

    err solved

--- src/components/ContactUsPage/Contact/index.jsx
+++ src/components/ContactUsPage/Contact/index.jsx
@@ -31,34 +31,34 @@ const Contact = () => {
             educational consultant.
           </p>
 
-          <div class='my-6'>
+          <div className='my-6'>
             <input
               type='text'
-              class='shadow-sm bg-gray-50 border-4 border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
+              className='shadow-sm bg-gray-50 border-4 border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
               placeholder='Your full name'
               required
             />
           </div>
-          <div class='mb-6'>
+          <div className='mb-6'>
             <input
               type='email'
-              class='shadow-sm bg-gray-50 border-4 border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
+              className='shadow-sm bg-gray-50 border-4 border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
               placeholder='example@example.com'
               required
             />
           </div>
-          <div class='mb-6'>
+          <div className='mb-6'>
             <input
               type='text'
-              class='shadow-sm bg-gray-50 border-4 border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
+              className='shadow-sm bg-gray-50 border-4 border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
               placeholder='Subject'
               required
             />
           </div>
-          <div class='mb-6'>
+          <div className='mb-6'>
             <textarea
               rows='4'
-              class='block p-2.5 w-full text-sm text-gray-900 bg-gray-50  border-4 border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50  border-4 border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
               placeholder=' Write  Your message'
               required
             ></textarea>
--- src/components/HomePage/Subscribe/index.jsx
+++ src/components/HomePage/Subscribe/index.jsx
@@ -4,12 +4,12 @@ import Image from 'next/image'
 
 const Subscribe = () => {
   return (
-    <div  className=' border-t-2'>
+    <div className=' border-t-2'>
       <div
         data-aos-delay='800'
         data-aos='flip-left'
         data-aos-duration='1500'
-        class='md:flex md:justify-between mx-auto w-full max-w-screen-xl items-center py-14 '
+        className='md:flex md:justify-between mx-auto w-full max-w-screen-xl items-center py-14 '
       >
         <div className=' md:flex items-center gap-10 px-5'>
           <div className='text-center'>
@@ -27,22 +27,22 @@ const Subscribe = () => {
           data-aos-duration='1500'
           data-aos='flip-right'
         >
-          <form class=' md:flex gap-4 items-center px-6 md:px-0'>
-            <label for='simple-search' class='sr-only'>
+          <form className=' md:flex gap-4 items-center px-6 md:px-0'>
+            <label htmlFor='simple-search' className='sr-only'>
               Search
             </label>
-            <div class='w-full'>
+            <div className='w-full'>
               <input
                 type='text'
                 id='simple-search'
-                class='bg-gray-50 md:w-[300px] my-5 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block  w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                className='bg-gray-50 md:w-[300px] my-5 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block  w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                 placeholder='Enter your email'
                 required
               />
             </div>
             <button
               type='submit'
-              class='p-2.5 w-full   text-sm font-medium text-white bg-blue-700 rounded-full border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              className='p-2.5 w-full   text-sm font-medium text-white bg-blue-700 rounded-full border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
             >
               Subscribe
             </button>
